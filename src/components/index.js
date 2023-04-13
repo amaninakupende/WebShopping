1e31ab5504383908b9b026a863f123cbd5745822
amaninakupende <1406777680@qq.com>
2023-04-13 14:29:35 +0800

    改变全局组件导入方式

--- src/components/index.js
+++ src/components/index.js
@@ -1,11 +1,24 @@
 //导入要注册的全局组件
-const modules = import.meta.glob('./**/index.vue');
-import { defineAsyncComponent } from "vue";
+// const modules = import.meta.glob('./**/index.vue');
+// console.log('+++', modules);
 
+// import { defineAsyncComponent } from "vue";
+
+// export default function install(app) {
+//   Object.keys(modules).forEach(path => {
+//     let pathArr = path.split('/');
+//     let name = pathArr[pathArr.length - 2];
+//     app.component(name ,defineAsyncComponent(modules[path]));
+//   })
+// }
+
+//导入要注册的全局组件
+const importModules = import.meta.globEager('./**/index.vue')
 export default function install(app) {
-  Object.keys(modules).forEach(path => {
-    let pathArr = path.split('/');
-    let name = pathArr[pathArr.length - 2];
-    app.component(name ,defineAsyncComponent(modules[path]));
+  Object.keys(importModules).forEach(path => {
+    let name = path.substring(path.indexOf('/') + 1, path.lastIndexOf('/'))
+    importModules[name] = importModules[path].default
+    delete importModules[path]
+    app.component(name, importModules[name])
   })
 }
